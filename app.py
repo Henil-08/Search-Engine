2f8a866bcab5644aaaddbb80349627b3fae29d88
Henil Gajjar <87967508+Henil-08@users.noreply.github.com>
2024-07-19 19:26:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from langchain.callbacks import StreamlitCallbackHandler
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
 ## Langsmith Tracking
 os.environ["GROQ_API_KEY"]=os.getenv("GROQ_API_KEY")
 ## Arxiv and wikipedia Tools
