bdb5341bde0ab068ed96a08503dab551ee9c7eee
“Henil <henilgajjar08@gmail.com>
2024-07-19 17:29:06 -0400

    final

--- app.py
+++ app.py
@@ -26,5 +26,24 @@ st.title("Chat Llama3 with search")
 
 if "messages" not in st.session_state:
     st.session_state["messages"]=[
-        {"role":"assisstant","content":"Hi,I'm a chatbot who can search the web. How can I help you?"}
+        {"role":"assistant", "content": "Hi, I'm a chatbot who can search the web. How can I help you?"}
     ]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg['content'])
+
+# Chat
+if prompt:=st.chat_input(placeholder="What is machine learning?"):
+    st.session_state.messages.append({"role":"user", "content":prompt})
+    st.chat_message("user").write(prompt)
+
+    llm=ChatGroq(model_name="Llama3-8b-8192",streaming=True)
+    tools=[search, arxiv, wiki]
+
+    search_agent=initialize_agent(tools,llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,handling_parsing_errors=True)
+
+    with st.chat_message("assistant"):
+        st_cb=StreamlitCallbackHandler(st.container(),expand_new_thoughts=False)
+        response=search_agent.run(st.session_state.messages,callbacks=[st_cb])
+        st.session_state.messages.append({'role':'assistant',"content":response})
+        st.write(response)
